34a74ce23588c941a7a47e154a5367fb4878c16b
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-05 23:22:38 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,22 @@
+from config import Load
+import os
+HOME = os.environ['HOME']
+
+def touchplaces():
+        places = Load("/var/tmp/places")
+        userdirs = []
+        with open ("{}{}".format(HOME, "/.config/user-dirs.dirs")) as dirs:
+                lines = dirs.read()
+        for test in lines.splitlines():
+                if test.startswith ("#") is not True:
+                        start = test.find("$") + 1
+                        end = test.find('"')
+                        dir = test[start:end-start + 1].replace("HOME", 
+                        "~")
+                        places.set("{}".format(dir), 
+                                "{},{},{}".format(os.path.basename(dir), 
+                                "Folder", 
+                                True))
+                                
+
+touchplaces()
